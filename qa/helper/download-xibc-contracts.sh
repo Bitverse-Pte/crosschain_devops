32720d0627230e05485096a7c1a437b0874a6640
wenbin <wenbin.shi@gmail.com>
2022-03-18 14:58:03 +0800

    update download-xibc-contracts.sh

--- qa/helper/download-xibc-contracts.sh
+++ qa/helper/download-xibc-contracts.sh
@@ -2,16 +2,10 @@
 
 function compileErc20(){
   cd xibc-contracts/erc20 && yarn && yarn compile
-
-  mv hardhat.config.ts hardhat.config.ts.bak
-  cp -r ../../../hardhat-config/hardhat.config.erc20.ts hardhat.config.ts
 }
 
 function compileEvm(){
   cd ../evm && yarn && yarn compile
-
-  mv hardhat.config.ts hardhat.config.ts.bak
-  cp -r ../../../hardhat-config/hardhat.config.evm.ts hardhat.config.ts
 }
 
 rm -rf xibc-contracts
